24a10698385fb4fca707acf906b62ca8e972d612
Tim Kessler <tkessler@consoloservices.com>
2020-01-03 14:25:48 -0500

    add version

--- upgrade-9.6-11.5-mac.sh
+++ upgrade-9.6-11.5-mac.sh
@@ -1,2 +1,4 @@
+# This script will upgrade a postgres cluster running on Mac Postgresql 9.6 to Mac Postgresql 11.5
+
 PGPASSWORD=postgres /usr/local/Cellar/postgresql/11.5_1/bin/pg_upgrade -U postgres -d /Library/PostgreSQL/9.6/data -D /Library/PostgreSQL/11.5/data -b /Library/PostgreSQL/9.6/bin/  -B /usr/local/Cellar/postgresql/11.5_1/bin/ -v
 
