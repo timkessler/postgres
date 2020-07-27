cdbde30041c28d707d974035b57cd3b003cf463b
Tim Kessler <tim.kessler@wellsky.com>
2020-07-27 08:34:43 -0400

    table size

---
+++ tables/size.sql
@@ -0,0 +1,19 @@
+SELECT table_name,
+	pg_size_pretty(total_bytes) AS total
+    , pg_size_pretty(index_bytes) AS index
+  FROM (
+		  SELECT *, total_bytes-index_bytes-coalesce(toast_bytes,0) AS table_bytes 
+		  FROM (
+		      SELECT c.oid,nspname AS table_schema, relname AS table_name
+		              , c.reltuples AS row_estimate
+		              , pg_total_relation_size(c.oid) AS total_bytes
+		              , pg_indexes_size(c.oid) AS index_bytes
+		              , pg_total_relation_size(reltoastrelid) AS toast_bytes
+		          FROM pg_class c
+		          LEFT JOIN pg_namespace n ON n.oid = c.relnamespace
+		          WHERE relkind = 'r' 
+		          -- and relname = 'your_table_name'
+			) t1
+	) t2
+order by index_bytes desc;
+
