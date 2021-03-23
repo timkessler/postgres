44cecefa566301d628bd83f846feb1f9e026da7e
Tim Kessler <tim.kessler@wellsky.com>
2021-03-23 14:49:45 -0400

    missing primary keys

---
+++ tables_missing_primary_keys.sql
@@ -0,0 +1,11 @@
+select tbl.table_schema, 
+       tbl.table_name
+from information_schema.tables tbl
+where table_type = 'BASE TABLE'
+  and table_schema not in ('pg_catalog', 'information_schema')
+  and not exists (select 1 
+                  from information_schema.key_column_usage kcu
+                  where kcu.table_name = tbl.table_name 
+                    and kcu.table_schema = tbl.table_schema);
+
+
